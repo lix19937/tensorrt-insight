81b39f4ad4e2bdf7856a10adf6e0b0b403719257
lix19937 <38753233+lix19937@users.noreply.github.com>
2024-07-18 23:20:16 +0800

    Update batch_run.sh

--- deploy/batch_run.sh
+++ deploy/batch_run.sh
@@ -2,19 +2,23 @@
 # bash  batch_run.sh   
 #
 
+#!/bin/bash 
+
+# /opt/deeproute/driver/config/starter/all_config.jsonnet
+
 #!/bin/bash
 
 data_pairs=(
-    "mnist.onnx,best"
+     "mnist.onnx,best"
     # "BV_PSD_P01_V02_20240619_TEST.onnx,fp16" 
-    # "BV_SEG_P01_V00_20240425.onnx,best"
-    # "bevf-160-200-20-20-1_simp.onnx,best"
-    # "bevf-320-400-20-20-1_simp.onnx,fp16"
-    # "bevf-320-400-20-20-1_simp_ops11_1_input.onnx,best"
-    # "bevf-64-160-20-20-1_poly.onnx,best"
-    # "bevformer_tiny_resnet18_poly.onnx,best"
-    # "mobilenetv2-sim.onnx,best"
-    # "yolov5s_poly.onnx,best"
+    #"BV_SEG_P01_V00_20240425.onnx,best"
+    #"bevf-160-200-20-20-1_simp.onnx,best"
+    #"bevf-320-400-20-20-1_simp.onnx,fp16"
+    #"bevf-320-400-20-20-1_simp_ops11_1_input.onnx,best"
+    #"bevf-64-160-20-20-1_poly.onnx,best"
+    #"bevformer_tiny_resnet18_poly.onnx,best"
+    #"mobilenetv2-sim.onnx,best"
+    #"yolov5s_poly.onnx,best"
 )
 
 ONNX_DIR=/ota
@@ -25,11 +29,9 @@ for pair in "${data_pairs[@]}"; do
     echo $filename, $type
 
     sonfile_name=$(basename $filename)
-    echo $sonfile_name
     prefix=$(echo "$sonfile_name" | awk -F'.' '{print $1}')
     echo $prefix
     base_name=$prefix
-    echo ${base_name}
 
     /usr/src/tensorrt/bin/trtexec --onnx=$ONNX_DIR/$filename \
     --saveEngine=$ONNX_DIR/${base_name}.plan \
@@ -39,3 +41,7 @@ for pair in "${data_pairs[@]}"; do
     --noDataTransfers --useCudaGraph --useSpinWait  --separateProfileRun \
     2>&1 | tee $ONNX_DIR/${base_name}.log
 done
+
+echo "All done"   
+
+
