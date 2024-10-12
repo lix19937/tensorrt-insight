902fa7ff28d1aab6ba857b7406e7c7ac9b0c2fa1
lix19937 <38753233+lix19937@users.noreply.github.com>
2024-10-12 13:22:53 +0800

    Update and rename tool/utils/plugins/list_plugins.py to tool/list_plugins.py

--- tool/list_plugins.py
+++ tool/list_plugins.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2020 NVIDIA Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# lix19937
 
 import ctypes
 import logging
@@ -57,6 +45,7 @@ def get_all_plugin_names(plugin_registry):
 #   (1) python list_plugins.py 
 #   (2) python list_plugins.py --plugins CustomIPluginV2/CustomPlugin.so
 #   (3) python list_plugins.py --plugins CustomIPluginV2/CustomPlugin.so /mnt/TensorRT/build/out/libnvinfer_plugin.so
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Script to list registered TensorRT plugins. Can optionally load custom plugin libraries.")
