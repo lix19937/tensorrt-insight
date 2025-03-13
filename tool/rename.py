a8589acad3fce1a409ab71a89bf0d233e27300a2
lix19937 <38753233+lix19937@users.noreply.github.com>
2025-03-13 10:34:13 +0800

    Update rename.py

--- tool/rename.py
+++ tool/rename.py
@@ -1,5 +1,5 @@
 
-# from char-ordered file to number-ordered file
+# step 1, from char-ordered file to number-ordered file
 
 import os
 filePath = 'result_yr_best3_ego/'
@@ -10,5 +10,8 @@ for it in fs:
   os.rename(filePath + it,  dst_dir + str(idx) + ".jpg")
   idx +=1
 
+
+# step 2,  img2video   
+# ffmpeg -r 10 -f image2 -i %d.jpg output1.mp4
 
 
