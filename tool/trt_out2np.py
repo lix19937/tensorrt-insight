de003f8b4b8e77d84ad18194f85826b9ae6ed842
lix19937 <38753233+lix19937@users.noreply.github.com>
2025-04-11 13:50:30 +0800

    Update trt_out2np.py

--- tool/trt_out2np.py
+++ tool/trt_out2np.py
@@ -1,1 +1,28 @@
+import numpy as np
+import json
+import os 
+
+# tensorrt output by --dumpOutput 
+directory_path = "output_tensor/"
+file_names = os.listdir(directory_path)
+
+icnt = 0
+for file in file_names:
+  with open(directory_path + file, "r") as f:
+      output_data = json.load(f)
+
+  out_dir = f"output_tensor_npy/{icnt}/"
+  if not os.path.exists(out_dir):
+      os.makedirs(out_dir)
+
+  idx = 0
+  for it in output_data:
+    print(f" {idx} {it.keys()}  {it['name']}  {type(it['values'])}" )
+    np_array = np.array(it["values"])  
+    np.save(f"{out_dir}{it['name']}.npy", np_array)
+    idx+=1
+
+  icnt+=1
+
+print("done")
 
